4d0c6a8afede9602a6467f1031aae98240b302c9
erwinLino <erwinlino011@gmail.com>
2025-10-14 00:36:30 +0200

    Logger featuring

--- backend/src/main/java/com/ecommerce/backend/rest_controllers/WebUserController.java
+++ backend/src/main/java/com/ecommerce/backend/rest_controllers/WebUserController.java
@@ -3,8 +3,9 @@ package com.ecommerce.backend.rest_controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebUserController {
 
+    private static final Logger log = LoggerFactory.getLogger(WebUserController.class);
     private final JdbcTemplate jdbc;
 
     public WebUserController(JdbcTemplate jdbc) {
@@ -26,28 +28,29 @@ public class WebUserController {
 
     @PostMapping("/web_user")
     public boolean tryToRegister(@RequestBody Map<String, Object> newUser) {
-        System.out.println("------");
-        System.out.println("------");
-        System.out.println(" INFO-> : " + newUser);
-        try {
-            //NEED TO ENCRYP INFORMATION FOR SECURUTY
-            String password = String.valueOf(newUser.get("password"));
-            BCryptPasswordEncoder encoder= new BCryptPasswordEncoder();
-            String encodePassWord=encoder.encode(password);
-            String sql = """
-            INSERT INTO web_user (name, last_name, email, password, created)
-            VALUES (?, ?, ?, ?, NOW()) """;
-            jdbc.update(sql,
-                    newUser.get("firstName"),
-                    newUser.get("lastName"),
-                    newUser.get("email"),
-                    encodePassWord
-            );
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+        // TRY CATH ARE COMMENTED , WE NEED TO DEBUG 'newUser' 
+         log.info("VER ESTO HOLLAA -> {} ", newUser);
+    
+        return true;
+        // try {
+        //     //NEED TO ENCRYP INFORMATION FOR SECURUTY
+        //     String password = String.valueOf(newUser.get("password"));
+        //     BCryptPasswordEncoder encoder= new BCryptPasswordEncoder();
+        //     String encodePassWord=encoder.encode(password);
+        //     String sql = """
+        //     INSERT INTO web_user (name, last_name, email, password, created)
+        //     VALUES (?, ?, ?, ?, NOW()) """;
+        //     jdbc.update(sql,
+        //             newUser.get("firstName"),
+        //             newUser.get("lastName"),
+        //             newUser.get("email"),
+        //             encodePassWord
+        //     );
+        //     return true;
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        //     return false;
+        // }
     }
 
 }
