0a4d7f6a148a1e77df045d4e793f0f488e53222c
erwinLino <erwinlino011@gmail.com>
2026-01-10 16:04:06 +0100

    fix

--- frontend/src/app/environment/environment.prod.ts
+++ frontend/src/app/environment/environment.prod.ts
@@ -1,4 +1,3 @@
-// API BASE . ES LA URL DE NUESTRO BACKEND COMO RAIZ 
 export const environment = {
   production: true,
   apiBase: 'https://ecommerce-java-ts.onrender.com'
--- frontend/src/app/environment/environment.ts
+++ frontend/src/app/environment/environment.ts
@@ -1,5 +1,4 @@
-// API BASE . ES LA URL DE NUESTRO BACKEND COMO RAIZ 
 export const environment = {
-  production: false,
-  apiBase: 'https://ecommerce-java-ts.onrender.com'
+  production: false, 
+  apiBase: 'https://ecommerce-java-ts.onrender.com' 
 };
